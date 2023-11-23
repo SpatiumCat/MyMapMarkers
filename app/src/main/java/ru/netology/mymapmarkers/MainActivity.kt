9fb827195f7e2dedfb2e46dec7cdd2a369207f48
Vladimir Baryshnikov <baryshnikovvladimirv@gmail.com>
2023-11-23 23:09:15 +0200

    added yandex map

--- app/src/main/java/ru/netology/mymapmarkers/MainActivity.kt
+++ app/src/main/java/ru/netology/mymapmarkers/MainActivity.kt
@@ -2,10 +2,12 @@ package ru.netology.mymapmarkers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yandex.mapkit.MapKitFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MapKitFactory.setApiKey(BuildConfig.MAPKIT_API_KEY)
         setContentView(R.layout.activity_main)
     }
 }
